e468a785cf3d424cae8526ddf2a853c51b4a8842
David Duarte <david.duarte@mindera.com>
2021-02-13 22:32:27 +0000

    adding help command

--- SimpleRandomTeams/Commands/BasicCommand.cs
+++ SimpleRandomTeams/Commands/BasicCommand.cs
@@ -1,6 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace SimpleRandomTeams.Commands
 {
@@ -12,6 +14,31 @@ namespace SimpleRandomTeams.Commands
         {
             await ctx.TriggerTypingAsync();
             await ctx.RespondAsync("yo.");
+        }
+
+        [Command("help")]
+        [Description("List available commands.")]
+        public async Task Help(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            
+            var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Simple Team Generator",
+                    Timestamp = DateTimeOffset.Now,
+                    Color = new DiscordColor(0xFF6133)
+                };
+            
+            embed.AddField("map", "Generate a random map to play from the csgo scrim map pool.");
+            embed.AddField("teams", "Generate random teams with members in the current voice channel.");
+            embed.AddField("veto", "Picks one random member from each team to start a veto process.");
+            embed.AddField("ban", "Bans a map from the veto available maps.");
+            embed.AddField("split", "Split the generated teams to their individual team voice channels.");
+            embed.AddField("end", "Move team members to the original voice channel.");
+            embed.AddField("reset", "Reset in memory database.");
+            embed.AddField("yo", "Test if the bot is running.");
+            
+            await ctx.RespondAsync(embed: embed);
         }
         
         [Command("reset")]
