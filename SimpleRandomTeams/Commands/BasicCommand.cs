3277c76840c5159dc35077310c08d8602071c2a2
David Duarte <david.duarte@mindera.com>
2021-02-13 22:39:29 +0000

    fixing help commands

--- SimpleRandomTeams/Commands/BasicCommand.cs
+++ SimpleRandomTeams/Commands/BasicCommand.cs
@@ -16,7 +16,7 @@ namespace SimpleRandomTeams.Commands
             await ctx.RespondAsync("yo.");
         }
 
-        [Command("help")]
+        [Command("commands")]
         [Description("List available commands.")]
         public async Task Help(CommandContext ctx)
         {
@@ -29,14 +29,14 @@ namespace SimpleRandomTeams.Commands
                     Color = new DiscordColor(0xFF6133)
                 };
             
-            embed.AddField("map", "Generate a random map to play from the csgo scrim map pool.");
-            embed.AddField("teams", "Generate random teams with members in the current voice channel.");
-            embed.AddField("veto", "Picks one random member from each team to start a veto process.");
-            embed.AddField("ban", "Bans a map from the veto available maps.");
-            embed.AddField("split", "Split the generated teams to their individual team voice channels.");
-            embed.AddField("end", "Move team members to the original voice channel.");
-            embed.AddField("reset", "Reset in memory database.");
-            embed.AddField("yo", "Test if the bot is running.");
+            embed.AddField("!map", "Generate a random map to play from the csgo scrim map pool.");
+            embed.AddField("!teams", "Generate random teams with members in the current voice channel.");
+            embed.AddField("!veto", "Picks one random member from each team to start a veto process.");
+            embed.AddField("!ban", "Bans a map from the veto available maps.");
+            embed.AddField("!split", "Split the generated teams to their individual team voice channels.");
+            embed.AddField("!end", "Move team members to the original voice channel.");
+            embed.AddField("!reset", "Reset in memory database.");
+            embed.AddField("!yo", "Test if the bot is running.");
             
             await ctx.RespondAsync(embed: embed);
         }
